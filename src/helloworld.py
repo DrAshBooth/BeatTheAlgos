c87a5077fff461c415342ec3efd3edf7886ce768
Ash Booth <ash.booth@soton.ac.uk>
2013-02-07 21:25:47 +0000

    Crude zoom animation added using jscript to iterate through huge images

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,15 +1,35 @@
 # -*- coding: utf-8 -*-
 
 from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.ext import db
-import webapp2
 
-class MainPage(webapp2.RequestHandler):
- 
+import webapp2
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
+class BaseHandler(webapp2.RequestHandler):
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+    
+    def render(self, template, **kw):
+        self.response.out.write(self.render_str(template, **kw))
+
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+
+class MainPage(BaseHandler):
+    
+    def render_front(self, title="", art="", error=""):
+        self.render("front.html")
+        
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
- 
+        self.render_front()
+
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
  
 def main():
